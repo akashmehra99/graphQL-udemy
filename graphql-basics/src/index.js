0668fe46d18c3040d301b7c3e46305ab772f7eea
Akash Mehra <akash.mehra99@gmail.com>
2023-02-02 07:53:10 +0530

    Mutation for comments and code formatting

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,64 +1,77 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { createServer } from "node:http";
-import {v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 // Demo data
-const users = [{
-  id: '1',
-  name: 'Andrew',
-  email: 'andrew@example.com',
-  age: 27
-}, {
-  id: '2',
-  name: 'Sarah',
-  email: 'sarah@example.com'
-}, {
-  id: '3',
-  name: 'Mike',
-  email: 'mike@example.com'
-}]
+const users = [
+  {
+    id: "1",
+    name: "Andrew",
+    email: "andrew@example.com",
+    age: 27,
+  },
+  {
+    id: "2",
+    name: "Sarah",
+    email: "sarah@example.com",
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "mike@example.com",
+  },
+];
 
-const posts = [{
-  id: '10',
-  title: 'GraphQL 101',
-  body: 'This is how to use GraphQL...',
-  published: true,
-  author: '1'
-}, {
-  id: '11',
-  title: 'GraphQL 201',
-  body: 'This is an advanced GraphQL post...',
-  published: false,
-  author: '1'
-}, {
-  id: '12',
-  title: 'Programming Music',
-  body: '',
-  published: false,
-  author: '2'
-}]
+const posts = [
+  {
+    id: "10",
+    title: "GraphQL 101",
+    body: "This is how to use GraphQL...",
+    published: true,
+    author: "1",
+  },
+  {
+    id: "11",
+    title: "GraphQL 201",
+    body: "This is an advanced GraphQL post...",
+    published: false,
+    author: "1",
+  },
+  {
+    id: "12",
+    title: "Programming Music",
+    body: "",
+    published: true,
+    author: "2",
+  },
+];
 
-const comments = [{
-  id: '102',
-  text: 'This worked well for me. Thanks!',
-  author: '3',
-  post: '10'
-}, {
-  id: '103',
-  text: 'Glad you enjoyed it.',
-  author: '1',
-  post: '10'
-}, {
-  id: '104',
-  text: 'This did no work.',
-  author: '2',
-  post: '11'
-}, {
-  id: '105',
-  text: 'Nevermind. I got it to work.',
-  author: '1',
-  post: '11'
-}]
+const comments = [
+  {
+    id: "102",
+    text: "This worked well for me. Thanks!",
+    author: "3",
+    post: "10",
+  },
+  {
+    id: "103",
+    text: "Glad you enjoyed it.",
+    author: "1",
+    post: "10",
+  },
+  {
+    id: "104",
+    text: "This did no work.",
+    author: "2",
+    post: "11",
+  },
+  {
+    id: "105",
+    text: "Nevermind. I got it to work.",
+    author: "1",
+    post: "11",
+  },
+];
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
@@ -80,6 +93,7 @@ const typeDefs = `
     type Mutation {
       createUser(name: String!, email: String!, age: Int): User!
       createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
     }
 
     type Post {
@@ -111,9 +125,9 @@ const resolvers = {
     me: () => {
       return {
         id: 123098,
-        name: 'Akash Mehra',
-        email: 'akash.mehra99@gmail.com',
-        age: 30
+        name: "Akash Mehra",
+        email: "akash.mehra99@gmail.com",
+        age: 30,
       };
     },
     posts: (parent, args, ctx, info) => {
@@ -121,15 +135,19 @@ const resolvers = {
         return posts;
       }
       const query = args.query.toLowerCase();
-      return posts.filter((pos) => pos.title.toLowerCase().includes(query) || pos.body.toLowerCase().includes(query));
+      return posts.filter(
+        (pos) =>
+          pos.title.toLowerCase().includes(query) ||
+          pos.body.toLowerCase().includes(query)
+      );
     },
     post: () => {
       return {
-        id: '092',
-        title: 'GrapgQl 101',
-        body: '',
-        published: true
-      }
+        id: "092",
+        title: "GrapgQl 101",
+        body: "",
+        published: true,
+      };
     },
     comments: (parent, args, ctx, info) => comments,
   },
@@ -137,52 +155,67 @@ const resolvers = {
     author: (parent, args, ctx, info) => {
       const authorId = parent.author;
       return users.find((user) => user.id === authorId);
-    }
+    },
   },
   User: {
     posts: (parent, args, ctx, info) => {
       const userId = parent.id;
       return posts.filter((post) => post.author === userId);
-    }
+    },
   },
   Comment: {
     author: (parent, args, ctx, info) => {
       return users.find((user) => {
-        return user.id === parent.author
-      })
+        return user.id === parent.author;
+      });
     },
     post: (parent, args, ctx, info) => {
       return posts.find((post) => {
-        return post.id === parent.post
-      })
-    }
+        return post.id === parent.post;
+      });
+    },
   },
   Mutation: {
-    createUser : (parent, args, ctx, info) => {
+    createUser: (parent, args, ctx, info) => {
       const emailTaken = users.some((user) => user.email === args.email);
-      if (emailTaken) {        
-        throw new Error('Email already taken.');
+      if (emailTaken) {
+        throw new Error("Email already taken.");
       }
       const user = {
         id: uuidv4(),
-        ...args
-      }
+        ...args,
+      };
       users.push(user);
       return user;
     },
     createPost: (parent, args, ctx, info) => {
       const userExists = users.some((user) => user.id === args.author);
       if (!userExists) {
-        throw new Error('User not Found');
+        throw new Error("User not Found");
       }
       const post = {
         id: uuidv4(),
-        ...args
-      }
+        ...args,
+      };
       posts.push(post);
       return post;
-    }
-  }
+    },
+    createComment: (parent, args, ctx, info) => {
+      const userExists = users.some((user) => user.id === args.author);
+      const postExists = posts.some(
+        (post) => post.published && post.id === args.post
+      );
+      if (!userExists || !postExists) {
+        throw new Error("Unable to find user or post");
+      }
+      const comment = {
+        id: uuidv4(),
+        ...args,
+      };
+      comments.push(comment);
+      return comment;
+    },
+  },
 };
 
 const yoga = createYoga({
